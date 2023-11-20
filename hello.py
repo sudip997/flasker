7cf85a55682482c790cb58df09ea8663799bdb9f
sudip <sudippatil008@gmail.com>
2023-11-20 16:49:30 +0530

    added functionality

--- hello.py
+++ hello.py
@@ -1,21 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # create a flask instance
 app = Flask(__name__)
+# Add Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# Secret Key
+app.config['SECRET_KEY'] = "my favorite food is shaahi paneer"
+
+# Initialize the Database
+db = SQLAlchemy(app)
+
+# Craate Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    #Create a string
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+# Create a form class
+
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # create a route decorator
-
-
 @app.route('/')
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    return render_template("add_user.html")
 # def index():
 # 	return "<h1>Hello World !</h1>"
 def index():
     first_name = "Ravi"
     stuff = "This is <strong>Bold</strong> text"
     favorite_pizza = ['Pepperoni', 'Cheese', 'Mushrooms', 41]
-    return render_template("index.html", first_name=first_name, 
-                           stuff=stuff, 
-                           favorite_pizza=favorite_pizza)
+    return render_template("index.html", first_name=first_name, stuff=stuff, favorite_pizza=favorite_pizza)
+
 
 @app.route('/user/<name>')
 def user(name):
@@ -32,3 +62,17 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form submitted successfully")
+
+    return render_template("name.html", name=name, form=form)
